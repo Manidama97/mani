f91daa78a6eb4b1a48fd4824f00bd94553a696f9
manidama97 <manidama9890@gmail.com>
2022-10-01 13:24:44 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def name(request):
+    return HttpResponse('<h1>THIS IS Sumanth HE FULL  pichi</h1>')
+
+def     
